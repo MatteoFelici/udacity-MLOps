36cca39ad029f58a02e032ce5251fd296059d1f9
Matteo Felici <matteo.felici@facile.it>
2021-10-22 17:37:36 +0200

    [Clean Code Project] First commit - first version of churn_library.py

---
+++ clean-code-project/constants.py
@@ -0,0 +1,36 @@
+CAT_COLUMNS = [
+    'Gender',
+    'Education_Level',
+    'Marital_Status',
+    'Income_Category',
+    'Card_Category'
+]
+
+QUANT_COLUMNS = [
+    'Customer_Age',
+    'Dependent_count',
+    'Months_on_book',
+    'Total_Relationship_Count',
+    'Months_Inactive_12_mon',
+    'Contacts_Count_12_mon',
+    'Credit_Limit',
+    'Total_Revolving_Bal',
+    'Avg_Open_To_Buy',
+    'Total_Amt_Chng_Q4_Q1',
+    'Total_Trans_Amt',
+    'Total_Trans_Ct',
+    'Total_Ct_Chng_Q4_Q1',
+    'Avg_Utilization_Ratio'
+]
+
+ATTRITION_COLUMN = 'Attrition_Flag'
+TARGET_COLUMN = 'Churn'
+
+FIGSIZE = (20, 10)
+
+PARAM_GRID = {
+    'n_estimators': [200, 500],
+    'max_features': ['auto', 'sqrt'],
+    'max_depth': [4, 5, 100],
+    'criterion': ['gini', 'entropy']
+}
